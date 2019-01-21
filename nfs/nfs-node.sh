0371460cb3d4f08c95be825ae67befe3d79b669c
SQGE <30278569+SQGE@users.noreply.github.com>
2019-01-21 17:21:37 +0800

    Update nfs-node.sh

--- nfs/nfs-node.sh
+++ nfs/nfs-node.sh
@@ -1,8 +1,12 @@
- #安装配置 nfs
+#!/bin/bash
+
+#安装配置 nfs
 nfs='192.168.160.100'
 mkdir -p  /data/k8s && chmod 755 /data/k8s/
 yum -y install nfs-utils rpcbind
 mount -t nfs $nfs:/data/k8s /data/k8s
+grep "$nfs:/mnt /mnt            nfs     rw" /etc/fstab || echo "192.168.48.130:/mnt /mnt            nfs     rw" >> /etc/fstab 
+
 systemctl start rpcbind.service 
 systemctl enable rpcbind.service 
 systemctl start nfs.service    
