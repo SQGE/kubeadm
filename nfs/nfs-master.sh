960816fc6552239ebf14dd3bf2dc07aca90ed16e
SQGE <30278569+SQGE@users.noreply.github.com>
2019-01-21 17:27:53 +0800

    Update nfs-master.sh

--- nfs/nfs-master.sh
+++ nfs/nfs-master.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+dir='/data/k8s/'
 #安装配置 nfs
-mkdir -p  /data/k8s && chmod 755 /data/k8s/
+mkdir -p $dir && chmod 755 $dir
 yum -y install nfs-utils rpcbind
-grep "/data/k8s  *(rw,sync,no_root_squash)" /etc/exports || echo "/data/k8s  *(rw,sync,no_root_squash)" >> /etc/exports
-systemctl start rpcbind.service && systemctl enable rpcbind && systemctl status rpcbind
+grep "$dir *(rw,sync,no_root_squash)" /etc/exports || echo "$dir  *(rw,sync,no_root_squash)" >> /etc/exports
+
+
 #启动 nfs 服务
-systemctl start nfs.service && systemctl enable nfs && systemctl status nfs
+systemctl start rpcbind.service 
+systemctl enable rpcbind 
+systemctl status rpcbind
+
+systemctl start nfs.service 
+systemctl enable nfs 
+systemctl status nfs
+
+#检查nfs是否正常
 rpcinfo -p|grep nfs
 cat /var/lib/nfs/etab
